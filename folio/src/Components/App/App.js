19890288228e90f470224b3d81e69597cd807e86
Mike Murphy <murph517@gmail.com>
2024-08-26 17:10:11 -0700

    Component js and css file creation

---
+++ folio/src/Components/App/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App/App.css';
+import About from '../About/About';
+import Card from '../Card/Card';
+import Pabst from '../Pabst/Pabst';
+import Random from '../Random/Random';
+import Social from '../Social/Social';
+import Video from '../Video/Video';
+import Work from '../Work/Work';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
